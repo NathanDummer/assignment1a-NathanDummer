0ee7b3282f0784a6770da27173c9a25cfdb149f9
Nathan Dummer <nathanedummer@gmail.com>
2024-09-12 09:30:03 -0400

    Updated write_ids

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "NathanDummer";
+    char STUDENT_ID[] = "919017571";
 
     return 0 ;
 }
